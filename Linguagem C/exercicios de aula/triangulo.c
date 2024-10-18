8efec32c0931f33d731dc066d10b128b90d97648
PedroYokada <pedro_hlyokada@outlook.com>
2024-10-18 00:11:28 -0300

    Fazendo exercicios em Linguagem C 18/10.

---
+++ Linguagem C/exercicios de aula/triangulo.c
@@ -0,0 +1,17 @@
+# include <stdio.h>
+
+int main()
+{
+  
+double area,base,altura;
+
+base = 12.56;
+altura = 67.89;
+
+area = base * altura;
+
+printf("O valor da area do triangulo é de: %.2lf",area);
+
+return 0;
+  
+}

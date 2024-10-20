c310c74f3f1735e2272c113a41f0f9b60b9cfff4
PedroYokada <pedro_hlyokada@outlook.com>
2024-10-20 19:23:01 -0300

    Treinando soma em Linguagem C 20/10.

---
+++ Linguagem C/exercicios de aula/soma_DoisValores.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+  int valor1,valor2,valor3;
+
+
+      printf("Digite o valor 1: ");
+      scanf("%d", &valor1);
+
+      printf("Digite o valor 1: ");
+      scanf("%d", &valor2);
+
+
+      valor3 = valor1 + valor2;
+
+      printf("SOMA = %d\n", valor3);
+
+
+
+      return 0;
+}

aebdc3bf1db5632fa9458a78d5bd64f40abc6b8f
PedroYokada <pedro_hlyokada@outlook.com>
2024-10-20 19:42:22 -0300

    Arrumando o exercicio de soma em Linguagem C 20/10.

--- Linguagem C/exercicios de aula/soma_DoisValores.c
+++ Linguagem C/exercicios de aula/soma_DoisValores.c
@@ -1,24 +1,26 @@
-#include <stdio.h>
-#include <string.h>
+# include <stdio.h>
+# include <string.h>
+
+void limpar_entrada() {
+    char c;
+    while ((c = getchar()) != '\n' && c != EOF) {}
+}
 
 int main()
 {
 
-  int valor1,valor2,valor3;
-
-
-      printf("Digite o valor 1: ");
-      scanf("%d", &valor1);
+  double valor1,valor2,valor3;
 
       printf("Digite o valor 1: ");
-      scanf("%d", &valor2);
+      scanf("%lf", &valor1);
+      limpar_entrada();
 
+      printf("Digite o valor 2: ");
+      scanf("%lf", &valor2);
+      limpar_entrada();
 
       valor3 = valor1 + valor2;
-
-      printf("SOMA = %d\n", valor3);
-
-
+      printf("SOMA = %.2lf\n", valor3);
 
       return 0;
 }

6d8a058a3351dfb627c806ed0313be26e7c305bb
PedroYokada <pedro_hlyokada@outlook.com>
2024-10-21 20:51:25 -0300

    adicionando exercicio aprovado em Linguagem C 21/10.

---
+++ Linguagem C/exercicios de aula/aprovado.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <string.h>
+
+int main()
+{
+
+    double nota1,nota2,media;
+
+    printf("Digite a primeira nota: ");
+    scanf("%lf", &nota1);
+
+
+    printf("Digite a segunda nota: ");
+    scanf("%lf", &nota2);
+
+    media = (nota1 + nota2)/2;
+
+    if (media >= 6)
+    {
+        printf("Aprovado!\n");
+    }
+    else
+    {
+        printf("Reprovado!\n");
+    }
+    return 0;
+}
--- Linguagem C/exercicios de aula/soma_DoisValores.c
+++ Linguagem C/exercicios de aula/soma_DoisValores.c
@@ -9,18 +9,18 @@ void limpar_entrada() {
 int main()
 {
 
-  double valor1,valor2,valor3;
+    double valor1,valor2,valor3;
 
-      printf("Digite o valor 1: ");
-      scanf("%lf", &valor1);
-      limpar_entrada();
+    printf("Digite o valor 1: ");
+    scanf("%lf", &valor1);
+    limpar_entrada();
 
-      printf("Digite o valor 2: ");
-      scanf("%lf", &valor2);
-      limpar_entrada();
+    printf("Digite o valor 2: ");
+    scanf("%lf", &valor2);
+    limpar_entrada();
 
-      valor3 = valor1 + valor2;
-      printf("SOMA = %.2lf\n", valor3);
+    valor3 = valor1 + valor2;
+    printf("SOMA = %.2lf\n", valor3);
 
-      return 0;
+    return 0;
 }

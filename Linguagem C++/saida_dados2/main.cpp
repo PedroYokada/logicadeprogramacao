02a7763c4a774ba1bba50d232d0b4cbfbc17f921
PedroYokada <pedro_hlyokada@outlook.com>
2024-11-03 19:53:46 -0300

    Exercicio de saída de dados C++

---
+++ Linguagem C++/saida_dados2/main.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+     cout<<"Bom dia!\n";
+     cout<<"Boa tarde!\n";
+	 cout << "Bom dia!!" << endl;
+	 cout << "Boa noite!!" << endl;
+	 cout << "\n" ;
+	 
+	 int x,y,z;
+	 
+	 x = 100;
+	 y = 200;
+	 
+	 z = x + y;
+	 
+	 cout << "O valor de x é de: " << x << endl;
+	 cout << "O valor de y é de: " << y << endl;
+	 cout << "O valor de z é de: " << z << endl;
+	 
+	 double a;
+	 a = 2.33333;
+	 cout << fixed << setprecision(2)<< "O valor de x é de: " << x << endl;
+	 
+	 int idade;
+     double salario;
+     string nome;
+     char sexo;
+     idade = 32;
+     salario = 4560.9;
+     nome = "Maria Silva";
+     sexo = 'F';
+     cout << fixed << setprecision(2);
+     cout << "A funcionaria " << nome << ", sexo " 
+     << sexo << ", ganha " << salario 
+     << " e tem " << idade << " anos" << endl;
+
+     return 0;
+}

0890346f1d1478598828c5183a51f193a19a2c96
PedroYokada <pedro_hlyokada@outlook.com>
2024-11-04 18:35:38 -0300

    Casting em C++ 04/11

---
+++ Linguagem C++/Casting/main.cpp
@@ -0,0 +1,31 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+ 
+    int a, b;
+    double resultado;
+    a = 5;
+    b = 2;
+    resultado = (double) a / b;
+    cout << resultado << endl;
+    
+    double x;
+    int y;
+    x = 5.0;
+    y = (int) x;
+    cout << y << endl; 
+
+    
+    return 0;
+}

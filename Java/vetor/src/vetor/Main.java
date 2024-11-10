dceec0387af94bccc3931617081524b2968ea17b
PedroYokada <pedro_hlyokada@outlook.com>
2024-11-10 00:10:32 -0300

    Exercicio de vetor em Java corrigido - Lógica de programação 10/11.

--- Java/vetor/src/vetor/Main.java
+++ Java/vetor/src/vetor/Main.java
@@ -3,16 +3,32 @@ package vetor;
 import java.util.Scanner;
 import java.util.Locale;
 
-
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
+
+		int N;
+
+		System.out.print("Quantos numeros voce vai digitar ? ");
+		N = sc.nextInt();
+
+		double[] vet = new double[N];
+
+		for (int i = 0; i < N; i++) {
+			System.out.print("Digite um numero: ");
+			vet[i] = sc.nextDouble();
+		}
 		
+		System.out.println();
+		System.out.println("NUMEROS DIGITADOS:");
 		
-		
+		for (int i = 0; i < N; i++) {
+			System.out.println(String.format("%.1f" ,vet[i]));
+		}
+
 		sc.close();
 
 	}
